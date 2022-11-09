4e370d3be24a7a9edb14bca9b6de6d76eefd42b5
Amalaseeli <amalremi07@gmail.com>
2022-11-09 12:36:12 +0000

    Create methode for retreving data in to the dictonary

--- myprotein.py
+++ myprotein.py
@@ -8,6 +8,7 @@ import time
 class Scrapper(unittest.TestCase):
     def __init__(self):
         self.driver=webdriver.Chrome()
+        self.data_dict={"img":[],'product_name':'','price':'','flavour':[]  }
         
     def load_and_accept_cookies(self):
         driver=self.driver
@@ -16,6 +17,7 @@ class Scrapper(unittest.TestCase):
 
         '''After signup it is popping up I am not robot window. So , As we know its change every time we just close the window.
         '''
+        time.sleep(2)
         self.driver.find_element(By.ID, 'email').send_keys("amalremi07@gmail.com")
         self.driver.find_element(by=By.XPATH, value='//button[@class="emailReengagement_newsletterForm_submit"]').click()
         time.sleep(5)
@@ -75,29 +77,45 @@ class Scrapper(unittest.TestCase):
         # print(len(set(item_list)))  
         return item_list 
 
+    def filter_data():
+        pass
+    
     def retrive_data(self,link):
         driver=self.driver
-        dict_properties={}
         driver.maximize_window()
-        driver.get(link) 
+        driver.get(link)
+        img_list=[] 
+        # img_container=driver.find_element(By.CLASS_NAME, 'athenaProductImageCarousel_listItem')
+        # img_li=img_container.find_element(By.CLASS_NAME, "athenaProductImageCarousel_image").get_attribute('src')
+        # for img in img_li:
+        #     img_list.append(img)
+        
         img=driver.find_element(By.CLASS_NAME, "athenaProductImageCarousel_image").get_attribute('src')
-        dict_properties['img']=img
+        
         product_name=driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[3]/div[2]/div/div[1]/div[2]/div/h1').text
-        dict_properties['product_name']=product_name
+        #dict_properties['product_name']=product_name
         price=driver.find_element(By.XPATH,'//p[@class="productPrice_price  "]').text
-        dict_properties['price']=price
+        #dict_properties['price']=price
         select_item=Select(driver.find_element(By.XPATH, '//*[@id="athena-product-variation-dropdown-5"]'))
         all_options=select_item.options
         flavour=[]
         for option in all_options:
             flavour.append(option.text)
-        dict_properties['flavour']=flavour
+        #dict_properties['flavour']=flavour
         time.sleep(3)
         # print(img)
         # print(product_name)
         # print(flavour)
-        return dict_properties
+        return img,product_name,price,flavour
 
+    def update_data_dict(self,link):
+            img_list,product_name,price,flavour=self.retrive_data(link)
+            self.data_dict['img_list']=img_list
+            self.data_dict['product_name']=product_name
+            self.data_dict['price']=price
+            self.data_dict['flavour']=flavour
+            return self.data_dict
+    
     def quit(self):
         self.driver.quit()
 
@@ -109,8 +127,9 @@ if __name__=="__main__":
     link_list.extend(webpage.get_links())
     for i in range(4):
         item_link=link_list[i]
-        item_properties.append(webpage.retrive_data(item_link))
+        item_properties.append(webpage.update_data_dict(item_link))
     print(item_properties)
+
     webpage.quit()
 
 
