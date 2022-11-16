da80cd34a4da4b87fc341f44b62f4139bbe0bd8a
Amalaseeli <amalremi07@gmail.com>
2022-11-16 19:47:17 +0000

    Run scrapper in headless mode

--- myprotein.py
+++ myprotein.py
@@ -46,6 +46,16 @@ class Scrapper:
      '''
 
     def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("start-maximized") # open Browser in maximized mode
+        options.add_argument('--headless')
+        options.add_argument("--disable-infobars") # disabling infobars
+        options.add_argument("--disable-extensions") # disabling extensions
+        options.add_argument("--disable-gpu") # applicable to windows os only
+        options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
+        options.add_argument("--no-sandbox") # Bypass OS security model
+        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36')
+        self.driver = webdriver.Chrome(options=options)
         self.driver=webdriver.Chrome()
         
     def load_and_accept_cookies(self):
